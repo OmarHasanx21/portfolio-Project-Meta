bdf9a9c9356fabfc1a798c1018b613068854728d
OmarHasanx21 <31017623+OmarHasanx21@users.noreply.github.com>
2026-01-22 01:32:54 +0200

    Add shopping cart section and enhance product item details with improved styling (v1.0) (Cart Not Working)

---
+++ Script.js
@@ -0,0 +1,14 @@
+function f(v) {
+  return document.getElementById(v);
+}
+
+let ProductList = [
+  { id: 1, name: "Diamond Ring", price: 19.99 },
+  { id: 2, name: "Golden Bracelet", price: 29.99 },
+  { id: 3, name: "Golden Earring", price: 39.99 },
+  { id: 4, name: "Golden Necklace", price: 49.99 },
+  { id: 5, name: "Golden Ring", price: 59.99 },
+  { id: 6, name: "Silver Bracelet", price: 69.99 },
+  { id: 7, name: "Silver Earring", price: 69.99 },
+  { id: 8, name: "Silver Ring", price: 69.99 },
+];
